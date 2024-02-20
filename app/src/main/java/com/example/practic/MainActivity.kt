cbc01043d8c3051edf1dce804983ce1f9bff824c
Jodrapj <cemapolyakov@gmail.com>
2024-02-20 16:38:47 +0300

    update ui 2

--- app/src/main/java/com/example/practic/MainActivity.kt
+++ app/src/main/java/com/example/practic/MainActivity.kt
@@ -1,17 +1,12 @@
 package com.example.practic
-
-import android.inputmethodservice.Keyboard
 import android.media.Image
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -19,36 +14,29 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Share
-import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.practic.ui.theme.PracticTheme
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+import java.util.Date
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,30 +47,56 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val post = Post()
-                    post.Draw("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus magna tincidunt consectetur lacus fringilla consequat aliquam maximus habitasse. Enim dapibus class dictumst lectus mauris est ipsum nostra id molestie arcu class. Habitant turpis cursus habitasse netus imperdiet egestas augue taciti.",
-                    10,10,10)
+                    val btpit = Author(painterResource(id = R.mipmap.ic_launcher_foreground), "BTPIT", getTimeNow())
+                    val post = Post("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus magna tincidunt consectetur lacus fringilla consequat aliquam maximus habitasse. Enim dapibus class dictumst lectus mauris est ipsum nostra id molestie arcu class. Habitant turpis cursus habitasse netus imperdiet egestas augue taciti.",
+                        10,10,10, btpit)
+                    post.Draw()
                 }
             }
         }
     }
 }
-
-class Author: Post { // Author class for post class top row.
-    var Avatar: Image
-    val Name: String
-    val PostTime: String
-    constructor(Avatar: Image, Name: String, PostTime: String) {
-        this.Avatar = Avatar
-        this.Name = Name
-        this.PostTime = PostTime
+@Composable
+fun getTimeNow(): String {
+    val month: String
+    val hourMinutes: String
+    when (LocalDateTime.now().month.value) {
+        1 -> month = stringResource(R.string.january)
+        2 -> month = stringResource(R.string.february)
+        3 -> month = stringResource(R.string.march)
+        4 -> month = stringResource(R.string.april)
+        5 -> month = stringResource(R.string.may)
+        6 -> month = stringResource(R.string.june)
+        7 -> month = stringResource(R.string.july)
+        8 -> month = stringResource(R.string.august)
+        9 -> month = stringResource(R.string.september)
+        10 -> month = stringResource(R.string.october)
+        11 -> month = stringResource(R.string.november)
+        12 -> month = stringResource(R.string.december)
+        else -> {
+            month = ""
+        }
     }
+    hourMinutes = LocalDateTime.now().hour.toString() + ":" + LocalDateTime.now().toLocalTime().minute
+    return LocalDateTime.now().dayOfMonth.toString() + " " + month + " " + hourMinutes
 }
-open class Post {
+class Author( // Author class for post class top row.
+    var Avatar: Painter,
+    val Name: String,
+    val PostTime: String
+)
+
+class Post(
+    val text: String,
+    val likes: Int,
+    val reposts: Int,
+    val views: Int,
+    val Author: Author
+) {
     @Composable
-    fun Draw(text: String, likes: Int, reposts: Int, views: Int) {
+    fun Draw() {
         val icons = Icons.Default
-        Column(
+        Column( // Main UI
             Modifier
                 .fillMaxWidth()
                 .padding(paddingValues = PaddingValues(10.dp, 20.dp, 10.dp, 20.dp))
@@ -94,6 +108,14 @@ open class Post {
                     .fillMaxWidth()
                     .height(Dp(80f))
             ) {
+                Image(
+                    painter = Author.Avatar,
+                    contentDescription = Author.Name
+                )
+                Column {
+                    Text(Author.Name, Modifier.padding(0.dp,18.dp,0.dp,0.dp))
+                    Text(Author.PostTime.toString())
+                }
             }
             Row( // Main row (Text.)
                 Modifier
