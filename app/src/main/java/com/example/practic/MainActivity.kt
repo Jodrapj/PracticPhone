9275c68ba3d93160aee4e3174700d45db187db8c
Jodrapj <cemapolyakov@gmail.com>
2024-02-21 13:51:59 +0300

    update

--- app/src/main/java/com/example/practic/MainActivity.kt
+++ app/src/main/java/com/example/practic/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.practic
+import android.content.res.Configuration
 import android.graphics.drawable.Icon
 import android.media.Image
 import android.os.Bundle
@@ -9,6 +10,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -31,12 +33,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.practic.ui.theme.PracticTheme
@@ -46,6 +50,7 @@ import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 import java.util.Date
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -206,7 +211,9 @@ class Post(
                         }
                     }
                 }
-                Column {
+                Column(
+                    horizontalAlignment = Alignment.End
+                ) {
                     Row {
                         val v1 : String = CurrentViews.value.toString()
                         Icon(icons.AccountCircle, contentDescription = "$views",
