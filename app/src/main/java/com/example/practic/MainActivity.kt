17055455b43e9ee61dd66b8113de7b23158f3906
Jodrapj <cemapolyakov@gmail.com>
2024-02-21 11:44:32 +0300

    update ui 4

--- app/src/main/java/com/example/practic/MainActivity.kt
+++ app/src/main/java/com/example/practic/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.practic
+import android.graphics.drawable.Icon
 import android.media.Image
 import android.os.Bundle
 import android.widget.ImageView
@@ -7,6 +8,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -16,18 +18,25 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconToggleButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.practic.ui.theme.PracticTheme
@@ -49,7 +58,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     val btpit = Author(painterResource(id = R.mipmap.ic_launcher_foreground), "BTPIT", getTimeNow())
                     val post = Post("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus magna tincidunt consectetur lacus fringilla consequat aliquam maximus habitasse. Enim dapibus class dictumst lectus mauris est ipsum nostra id molestie arcu class. Habitant turpis cursus habitasse netus imperdiet egestas augue taciti.",
-                        10,10,10, btpit)
+                        1000,1100,1000, btpit)
                     post.Draw()
                 }
             }
@@ -59,7 +68,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun getTimeNow(): String {
     val month: String
-    val hourMinutes: String
     when (LocalDateTime.now().month.value) {
         1 -> month = stringResource(R.string.january)
         2 -> month = stringResource(R.string.february)
@@ -77,7 +85,7 @@ fun getTimeNow(): String {
             month = ""
         }
     }
-    hourMinutes = LocalDateTime.now().hour.toString() + ":" + LocalDateTime.now().toLocalTime().minute
+    val hourMinutes: String = LocalDateTime.now().hour.plus(3).toString() + ":" + LocalDateTime.now().toLocalTime().minute
     return LocalDateTime.now().dayOfMonth.toString() + " " + month + " " + hourMinutes
 }
 class Author( // Author class for post class top row.
@@ -85,16 +93,17 @@ class Author( // Author class for post class top row.
     val Name: String,
     val PostTime: String
 )
-
 class Post(
     val text: String,
-    val likes: Int,
-    val reposts: Int,
-    val views: Int,
+    var likes: Int,
+    var reposts: Int,
+    var views: Int,
     val Author: Author
 ) {
     @Composable
     fun Draw() {
+        val likeChecked = remember { mutableStateOf(false)}
+        val CurrentReposts = remember { mutableStateOf(reposts)}
         val icons = Icons.Default
         Column( // Main UI
             Modifier
@@ -131,12 +140,85 @@ class Post(
                     .height(Dp(40f))
                     .padding(Dp(10f))
             ) {
-                Icon(icons.FavoriteBorder, stringResource(R.string.likes))
-                Text(text = " $likes")
-                Icon(icons.Share, stringResource(R.string.reposts))
-                Text(text = " $reposts")
-                Icon(icons.AccountCircle, stringResource(R.string.views), Modifier.padding(220.dp, 0.dp, 0.dp, 0.dp))
-                Text(text = " $views")
+                Column {// Likes click script
+                    Row {
+                        IconToggleButton(
+                            checked = likeChecked.value,
+                            onCheckedChange = {
+                                likeChecked.value = it
+                                if (likeChecked.value) {
+                                    likes++
+                                } else {
+                                    likes--
+                                }
+                            }) {
+                            if (likeChecked.value) {
+                                androidx.compose.material3.Icon(
+                                    imageVector = icons.Favorite,
+                                    contentDescription = "$likes"
+                                )
+                            } else {
+                                androidx.compose.material3.Icon(
+                                    imageVector = icons.FavoriteBorder,
+                                    contentDescription = "$likes"
+                                )
+                            }
+                        }
+                        if (likes < 999)
+                            Text(text = "$likes")
+                        else if (likes >= 1000) {
+                            if (likes < 1100) {
+                                val v1: String = likes.toString()
+                                Text(text = v1[0].toString() + "K")
+                            } else {
+                                val v1:String = likes.toString()
+                                Text(text = v1[0].toString() + "." + v1[1].toString() + "K")
+                            }
+                        }
+                    }
+                }
+                Column {// Reposts click script
+                    Row {
+                        IconButton( onClick = {
+                            CurrentReposts.value++
+                        }) {
+                            androidx.compose.material3.Icon(
+                                imageVector = icons.Share,
+                                contentDescription = "${CurrentReposts.value}"
+                            )
+                        }
+                        if (CurrentReposts.value <= 999)
+                            Text(text = "${CurrentReposts.value}")
+                        else if (CurrentReposts.value >= 1000) {
+                            if (CurrentReposts.value < 1100) {
+                                val v1: String = CurrentReposts.value.toString()
+                                Text(text = v1[0].toString() + "K")
+                            } else {
+                                val v1:String = CurrentReposts.value.toString()
+                                Text(text = v1[0].toString() + "." + v1[1].toString() + "K")
+                            }
+                        }
+                    }
+                }
+                Column {
+                    Row {
+                        Icon(icons.AccountCircle, contentDescription = "$views",
+                            Modifier
+                                .padding(180.dp, 0.dp, 0.dp, 0.dp)
+                                )
+                        if (views <= 999)
+                            Text(text = " $views")
+                        else {
+                            if (views < 1100) {
+                                val v1: String = views.toString()
+                                Text(text = v1[0].toString() + "K")
+                            } else {
+                                val v1:String = views.toString()
+                                Text(text = v1[0].toString() + "." + v1[1].toString() + "K")
+                            }
+                        }
+                    }
+                }
             }
         }
     }
